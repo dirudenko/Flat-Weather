5f6e66b2ab6b637cd3b2d1b0f7416965753abd7e
Dmitry Rudenko <dirudenko@icloud.com>
2021-12-15 10:32:58 +0300

    first commit

---
+++ MyApp/MyApp/Services/NetworkService.swift
@@ -0,0 +1,12 @@
+//
+//  NetworkService.swift
+//  MyApp
+//
+//  Created by Dmitry on 15.12.2021.
+//
+
+import Foundation
+
+final class NetworkService {
+  
+}
---
+++ MyApp/MyApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyApp
+//
+//  Created by Dmitry on 15.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
